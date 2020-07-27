cb0214c8cf08976b9632261cd4b81959b6d75bad
alomrani <mohammad.alomrani@mail.utoronto.ca>
2020-07-27 18:11:04 -0400

    random agent now works

--- corl/data/generate_data.py
+++ corl/data/generate_data.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from utils.data_utils import check_extension, save_dataset
 import networkx as nx
+from scipy.optimize import linear_sum_assignment
 
 
 def generate_tsp_data(dataset_size, tsp_size):
@@ -12,29 +13,39 @@ def generate_tsp_data(dataset_size, tsp_size):
 def generate_bipartite_data(
     dataset_size, u_size, v_size, num_edges, future_edge_weight, weights_range
 ):
-    G = []
-    D = []
-    W = []
+    G, D, E, W, M = [], [], [], [], []
     for i in range(dataset_size):
         g1 = nx.bipartite.gnmk_random_graph(u_size, v_size, num_edges)
+        d_old = np.array(sorted(g1.degree))[u_size:, 1]
+        c = nx.bipartite.biadjacency_matrix(
+            g1, range(0, u_size), range(u_size, u_size + v_size)
+        ).toarray() * np.random.randint(
+            weights_range[0], weights_range[1], (u_size, v_size)
+        )
         g1.add_node(-1, bipartite=0)
         g1.add_edges_from(list(zip([-1] * v_size, range(u_size, u_size + v_size))))
         d = np.array(sorted(g1.degree))[u_size + 1 :, 1]
         l1 = nx.line_graph(g1)
-        w = np.random.randint(weights_range[0], weights_range[1], num_edges + v_size)
-        w[
-            np.int_(np.sum(np.triu(np.ones((v_size, v_size))).T * d, axis=1) - 1)
-        ] = future_edge_weight
+        # w = np.random.randint(weights_range[0], weights_range[1], num_edges)
+        w = c[c.nonzero()]
+        # w[
+        #     np.int_(np.sum(np.triu(np.ones((v_size, v_size))).T * d, axis=1) - 1)
+        # ] = future_edge_weight
+        w = np.insert(w, np.cumsum(d_old) - d_old, future_edge_weight)
         # w = np.insert(w,np.sum(np.triu(np.ones((v_size,v_size))).T * d, axis=1), future_edge_weight)
-
         # add negative adjacency matrix and edge weights
-        order = np.argsort(np.array(l1.nodes)[:, 1], axis=None)
-        m = nx.convert_matrix.to_numpy_array(l1)
-        ordered_m = np.take(np.take(m, order, axis=1), order, axis=0)
-        G.append(list(-(ordered_m - 1)))
+        # order = np.argsort(np.array(l1.nodes)[:, 1], axis=None)
+        s = sorted(list(l1.nodes), key=lambda a: a[0])
+        s = sorted(s, key=lambda a: a[1])
+        m = nx.convert_matrix.to_numpy_array(l1, s)
+        # ordered_m = np.take(np.take(m, order, axis=1), order, axis=0)
+        G.append(list(-(m - 1)))
         W.append(list(w))
         D.append(list(d))
-    return G, W, D
+        E.append(s)
+        i1, i2 = linear_sum_assignment(c, maximize=True)
+        M.append(c[i1, i2].sum())
+    return G, W, D, E, M
 
 
 def generate_vrp_data(dataset_size, vrp_size):
--- corl/models/state_bipartite.py
+++ corl/models/state_bipartite.py
@@ -1,6 +1,7 @@
 import torch
 from typing import NamedTuple
-from utils.boolmask import mask_long2bool, mask_long_scatter
+
+# from utils.boolmask import mask_long2bool, mask_long_scatter
 import numpy as np
 import networkx as nx
 
@@ -11,10 +12,13 @@ TODO: CODE BELOW SHOULD BE MODIFIED TO WORK FOR BIPARTITE
 
 class StateBipartite(NamedTuple):
     # Fixed input
-    graphs: torch.Tensor  # full adjacency matrix of all graphs in a batch
+    graphs: torch.Tensor  # full adjacency matrix of all line graphs in a batch
+    # adj: torch.Tensor # full adjacency matrix of all graphs in a batch
     weights: torch.Tensor  # weights of all edges of each graph in a batch
     edges: torch.Tensor  # edges of each graph in a batch
     degree: torch.Tensor  # degree of each node in the V set
+    u_size: torch.Tensor
+    batch_size: torch.Tensor
     # If this state contains multiple copies (i.e. beam search) for the same instance, then for memory efficiency
     # the loc and dist tensors are not kept multiple times, so we need to use the ids to index the correct rows.
     ids: torch.Tensor  # Keeps track of original fixed data index of rows
@@ -45,53 +49,41 @@ class StateBipartite(NamedTuple):
                 size=self.size[key],
                 edges=self.edges[key],
                 degree=self.degree[key],
+                u_size=self.u_size[key],
             )
         return super(StateBipartite, self).__getitem__(key)
 
     @staticmethod
-    def initialize(graphs, u_size, num_edges, visited_dtype=torch.uint8):
+    def initialize(
+        graphs, weights, edges, degree, u_size, num_edges, visited_dtype=torch.uint8
+    ):
 
-        batch_size, n_loc, _ = graphs.size()
+        batch_size = len(graphs)
         # size = torch.zeros(batch_size, 1, dtype=torch.long, device=graphs.device)
         return StateBipartite(
-            graphs=torch.tensor(graphs[:, 0]),
-            weights=torch.tensor(graphs[:, 1]),
-            edges=torch.tensor(graphs[:, 2]),
-            degree=torch.tensor(graphs[:, 3]),
+            graphs=torch.tensor(graphs),
+            u_size=torch.tensor([u_size]),
+            weights=torch.tensor(weights),
+            edges=torch.tensor(edges),
+            degree=torch.tensor(degree),
+            batch_size=torch.tensor([batch_size]),
             ids=torch.arange(batch_size, dtype=torch.int64, device=graphs.device)[
                 :, None
             ],  # Add steps dimension
             # Keep visited with depot so we can scatter efficiently (if there is an action for depot)
             matched_nodes=(  # Visited as mask is easier to understand, as long more memory efficient
                 torch.zeros(
-                    batch_size, 1, u_size, dtype=torch.uint8, device=graphs.device
+                    batch_size, 1, u_size + 1, dtype=torch.uint8, device=graphs.device
                 )
-                if visited_dtype == torch.uint8
-                else torch.zeros(
-                    batch_size,
-                    1,
-                    (n_loc + 63) // 64,
-                    dtype=torch.int64,
-                    device=graphs.device,
-                )  # Ceil
             ),
             picked_edges=(  # Visited as mask is easier to understand, as long more memory efficient
                 torch.zeros(
                     batch_size, 1, num_edges, dtype=torch.uint8, device=graphs.device
                 )
-                if visited_dtype == torch.uint8
-                else torch.zeros(
-                    batch_size,
-                    1,
-                    (n_loc + 63) // 64,
-                    dtype=torch.int64,
-                    device=graphs.device,
-                )  # Ceil
             ),
             size=torch.zeros(batch_size, 1, device=graphs.device),
-            i=torch.zeros(
-                1, dtype=torch.int64, device=graphs.device
-            ),  # Vector with length num_steps
+            i=torch.ones(1, dtype=torch.int64, device=graphs.device)
+            * u_size,  # Vector with length num_steps
         )
 
     def get_final_cost(self):
@@ -103,12 +95,23 @@ class StateBipartite(NamedTuple):
 
     def update(self, selected):
         # Update the state
-        nodes = self.matched_nodes.scatter_(-1, selected[:, :-1], 1)
-        selected_u = selected.T.expand(self.edges.shape[1], -1)
-        selected_v = (self.i).T.expand(self.edges.shape[1], -1)
-        mask = (selected_u == self.edges[:, 0]) & (selected_v == self.edges[:, 1])
-        total_weights = self.size + torch.sum(self.weights * mask.T.long(), dim=1)
-        edges = self.picked_edges + mask.long()
+        nodes = self.matched_nodes.squeeze(1).scatter_(-1, selected, 1)
+        selected_u = (selected - 1).expand(self.batch_size, self.edges.shape[1])
+        # state.edges.shape
+        selected_v = (self.i).T.expand(selected_u.shape)
+        mask = (selected_u == self.edges[:, :, 0]) & (selected_v == self.edges[:, :, 1])
+        total_weights = (
+            self.size + torch.sum(self.weights * mask.long(), dim=1)[:, None]
+        )
+        edges = self.picked_edges.squeeze(1) + mask.long()
+        # nodes = self.matched_nodes.squeeze(1).scatter_(-1, selected, 1)
+        # selected_u = selected.T.expand(self.edges.shape[1], -1)
+        # selected_v = (self.i).T.expand(self.edges.shape[1], -1)
+        # mask = (selected_u == self.edges[:, :, 0].T) & (selected_v == self.edges[:, :, 1].T)
+        # total_weights = self.size + torch.sum(self.weights * mask.T.long(), dim=1)
+        # print(self.picked_edges)
+        # print(mask.long())
+        # edges = self.picked_edges + mask.long()
         # mask = edges.scatter_(-1, self.edges[:, :, 0] == )
         return self._replace(
             matched_nodes=nodes, size=total_weights, picked_edges=edges, i=self.i + 1,
@@ -116,15 +119,36 @@ class StateBipartite(NamedTuple):
 
     def all_finished(self):
         # Exactly n steps
-        return self.i.item() >= self.degree.size(-1)
+        return (self.i.item() - self.u_size.item()) >= self.degree.size(-1)
 
     def get_current_node(self):
         return self.i
 
     def get_mask(self):
+        self.matched_nodes[:, 0] = 0
+        a = self.edges.reshape(
+            self.edges.size(0) * self.edges.size(1), 2
+        ) == self.i.expand_as(
+            self.edges.reshape(self.edges.size(0) * self.edges.size(1), 2)
+        )
+        b = self.edges.reshape(self.edges.size(0) * self.edges.size(1), 2)[:, 0]
+        c = b[a[:, 1].nonzero()]
+        m = torch.zeros(c.size(0), self.matched_nodes.squeeze(1).size(1)).scatter_(
+            -1, c + 1, 1
+        )
+        f = torch.index_select(
+            m.cumsum(0),
+            0,
+            (self.degree[:, self.i.item() - self.u_size.item()]).cumsum(0) - 1,
+        )
+        mask = (
+            torch.cat((f, torch.zeros(1, f.size(1))), dim=0)
+            - torch.cat((torch.zeros(1, f.size(1)), f), dim=0)
+        )[:-1, :]
+        # print(mask)
         return (
-            self.matched_nodes > 0
-        )  # Hacky way to return bool or uint8 depending on pytorch version
+            self.matched_nodes.squeeze(1) + (1 - mask) > 0
+        ).long()  # Hacky way to return bool or uint8 depending on pytorch version
 
     def get_nn(self, k=None):
         # Insert step dimension
