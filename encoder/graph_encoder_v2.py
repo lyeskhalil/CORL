433224bf159f095f75c4051f0284580acd3136ad
alomrani <mohammad.alomrani@mail.utoronto.ca>
2020-11-09 11:44:03 -0500

    added GAT encoder

--- encoder/graph_encoder_v2.py
+++ encoder/graph_encoder_v2.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from torch import nn
 import math
+import torch.nn.functional as F
 
 
 class SkipConnection(nn.Module):
@@ -266,4 +267,139 @@ class GraphAttentionEncoder(nn.Module):
         return (
             h,  # (batch_size, graph_size, embed_dim)
             h.mean(dim=1),  # average to get embedding of graph, (batch_size, embed_dim)
+        )
+
+
+class GAT(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads, weights):
+        """Dense version of GAT."""
+        super(GAT, self).__init__()
+        self.dropout = dropout
+
+        self.attentions = [
+            GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True)
+            for _ in range(nheads)
+        ]
+        for i, attention in enumerate(self.attentions):
+            self.add_module("attention_{}".format(i), attention)
+
+        self.out_att = GraphAttentionLayer(
+            nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False
+        )
+
+    def forward(self, x, adj, weights):
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = torch.cat([att(x, adj, weights) for att in self.attentions], dim=1)
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.elu(self.out_att(x, adj))
+        return F.log_softmax(x, dim=1)
+
+
+class GraphAttentionLayer(nn.Module):
+    """
+    Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+    """
+
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+        super(GraphAttentionLayer, self).__init__()
+        self.dropout = dropout
+        self.in_features = in_features
+        self.out_features = out_features
+        self.alpha = alpha
+        self.concat = concat
+
+        self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))
+        nn.init.xavier_uniform_(self.W.data, gain=1.414)
+        self.a = nn.Parameter(torch.empty(size=(2 * out_features, 1)))
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+    def forward(self, h, adj, weights):
+        Wh = torch.mm(
+            h, self.W
+        )  # h.shape: (N, in_features), Wh.shape: (N, out_features)
+        a_input = self._prepare_attentional_mechanism_input(Wh)
+        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
+        batch_size, graph_size, input_dim = h.size()
+        v = graph_size - weights.size(2)
+        u = weights.size(2)
+        weights1 = torch.cat(
+            (
+                torch.zeros((batch_size, u, u), device=weights.device),
+                weights[:, :v, :].transpose(1, 2).float(),
+            ),
+            dim=2,
+        )
+        weights2 = torch.cat(
+            (
+                weights[:, :v, :].float(),
+                torch.zeros((batch_size, v, v), device=weights.device),
+            ),
+            dim=2,
+        )
+        weights = torch.cat((weights1, weights2), dim=1)
+        zero_vec = -9e15 * torch.ones_like(e)
+        attention = torch.where(adj > 0, e, zero_vec)
+        attention = attention.exp() * weights
+        attention = F.normalize(attention, dim=1, p=1)
+        attention = F.dropout(attention, self.dropout, training=self.training)
+        h_prime = torch.matmul(attention, Wh)
+
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+
+    def _prepare_attentional_mechanism_input(self, Wh):
+        N = Wh.size()[0]  # number of nodes
+
+        # Below, two matrices are created that contain embeddings in their rows in different orders.
+        # (e stands for embedding)
+        # These are the rows of the first matrix (Wh_repeated_in_chunks):
+        # e1, e1, ..., e1,            e2, e2, ..., e2,            ..., eN, eN, ..., eN
+        # '-------------' -> N times  '-------------' -> N times       '-------------' -> N times
+        #
+        # These are the rows of the second matrix (Wh_repeated_alternating):
+        # e1, e2, ..., eN, e1, e2, ..., eN, ..., e1, e2, ..., eN
+        # '----------------------------------------------------' -> N times
+        #
+
+        Wh_repeated_in_chunks = Wh.repeat_interleave(N, dim=0)
+        Wh_repeated_alternating = Wh.repeat(N, 1)
+        # Wh_repeated_in_chunks.shape == Wh_repeated_alternating.shape == (N * N, out_features)
+
+        # The all_combination_matrix, created below, will look like this (|| denotes concatenation):
+        # e1 || e1
+        # e1 || e2
+        # e1 || e3
+        # ...
+        # e1 || eN
+        # e2 || e1
+        # e2 || e2
+        # e2 || e3
+        # ...
+        # e2 || eN
+        # ...
+        # eN || e1
+        # eN || e2
+        # eN || e3
+        # ...
+        # eN || eN
+
+        all_combinations_matrix = torch.cat(
+            [Wh_repeated_in_chunks, Wh_repeated_alternating], dim=1
+        )
+        # all_combinations_matrix.shape == (N * N, 2 * out_features)
+
+        return all_combinations_matrix.view(N, N, 2 * self.out_features)
+
+    def __repr__(self):
+        return (
+            self.__class__.__name__
+            + " ("
+            + str(self.in_features)
+            + " -> "
+            + str(self.out_features)
+            + ")"
         )
