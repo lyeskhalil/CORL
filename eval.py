90430c614b4fc0981acecd97a59d58367c2fcd07
Reza-Mor <moravejm@DESKTOP-38F9F57.localdomain>
2020-12-28 09:03:36 -0800

    added pipeline

--- eval.py
+++ eval.py
@@ -50,13 +50,13 @@ def validate_many(opts, model, problem):
     for i, j in enumerate(
         np.arange(min_p, max_p, (min_p + max_p) / opts.eval_num_range)
     ):
-        dataset_folder = opts.eval_dataset + "/eval{}".format(i)
+        dataset_folder = opts.eval_dataset
 
         eval_dataset = problem.make_dataset(
             dataset_folder, opts.eval_size, opts.problem
         )
         eval_dataloader = DataLoader(
-            eval_dataset, batch_size=opts.eval_batch_size, num_workers=0
+            eval_dataset, batch_size=opts.eval_batch_size, num_workers=1
         )
 
         avg_ratio, cr, avg_cr = validate(model, eval_dataloader, opts)
@@ -85,16 +85,9 @@ def get_op_ratios(opts, model, problem):
     ops = []
     # for i in graph family parameters
     for i in range(len(opts.eval_set)):
-        # opts.eval_dataset + "eval/graphs/"
-        dataset_folder = opts.eval_dataset + "/{}_{}_{}_{}_{}_{}by{}".format(
-            opts.problem, opts.graph_family, opts.eval_set[i], 
-            opts.weight_distribution, opts.weight_distribution_param, 
-            opts.u_size, opts.v_size
-        ).replace(" ","")
-        
-        # get the eval data set as a pytorch dataset object 
+        # get the eval dataset as a pytorch dataset object 
         eval_dataset = problem.make_dataset(
-            dataset_folder, opts.eval_size, opts.eval_size, opts.problem, opts
+            opts.eval_dataset, opts.eval_size, opts.eval_size, opts.problem, opts
         )
         eval_dataloader = DataLoader(
             eval_dataset, batch_size=opts.eval_batch_size, num_workers=0
@@ -146,16 +139,15 @@ def plot_box(opts, data):
 
 def load_models(opts):
     """
-    Load models (here we refer to them as data) from load_path (if eval_model_paths is being used )
+    Load models (here we refer to them as data) from load_path 
     """
     load_data = {}
     load_datas = []
-    load_paths = opts.eval_model_paths if opts.eval_model_paths is not None else []
-    if load_paths is not None:
-        for path in load_paths:
-            print("  [*] Loading data from {}".format(path))
-            load_data = torch_load_cpu(path)
-            load_datas.append(load_data)
+    path = opts.load_path if opts.load_path is not None else []
+    if path is not None:
+        print("  [*] Loading data from {}".format(path))
+        load_data = torch_load_cpu(path)
+        load_datas.append(load_data)
     return load_datas
 
 
@@ -190,6 +182,7 @@ def load_ff_models(opts):
 
 
 def initialize_models(opts, models, load_datas):
+    problem = load_problem(opts.problem)
     for m in range(len(opts.eval_models)):
         model_class = {"attention": AttentionModel, "ff": FeedForwardModel}.get(
             opts.eval_models[m], None
@@ -197,7 +190,7 @@ def initialize_models(opts, models, load_datas):
         model = model_class(
             opts.embedding_dim,
             opts.hidden_dim,
-            problem=opts.problem,
+            problem=problem,
             n_encode_layers=opts.n_encode_layers,
             mask_inner=True,
             mask_logits=True,
@@ -221,11 +214,12 @@ def initialize_models(opts, models, load_datas):
 
 
 def initialize_attention_models(opts, attention_models, load_attention_datas):
+    problem = load_problem(opts.problem)
     for m in range(len(load_attention_datas)):
         model = AttentionModel(
             opts.embedding_dim,
             opts.hidden_dim,
-            problem=opts.problem,
+            problem=problem,
             n_encode_layers=opts.n_encode_layers,
             mask_inner=True,
             mask_logits=True,
@@ -249,11 +243,12 @@ def initialize_attention_models(opts, attention_models, load_attention_datas):
 
 
 def initialize_ff_models(opts, ff_models, load_ff_datas):
+    problem = load_problem(opts.problem)
     for m in range(len(load_ff_datas)):
         model = FeedForwardModel(
             opts.embedding_dim,
             opts.hidden_dim,
-            problem=opts.problem,
+            problem=problem,
             n_encode_layers=opts.n_encode_layers,
             mask_inner=True,
             mask_logits=True,
@@ -308,20 +303,21 @@ def run(opts):
         and opts.eval_attention_dir is None
     ) or opts.resume is None, "either one of load_path, eval_ff_dir, eval_attention_dir as well as resume should be given"
     
+    models = [] 
     # Initialize models
     if opts.load_path is not None:
         load_datas = load_models(opts)
-        models = []  # these are the models that are specified in by the file
+         # these are the models that are specified in by the file
         initialize_models(opts, models, load_datas)
     elif opts.eval_attention_dir is not None:
         load_attention_datas = load_attention_models(opts)
-        attention_models = []  # attention models from the directory
-        initialize_attention_models(opts, attention_models, load_attention_datas)
+        initialize_attention_models(opts, models, load_attention_datas) # attention models from the directory
     elif opts.eval_ff_dir is not None:
         load_ff_datas = load_ff_models(opts)
-        ff_models = []  # feed forwad models from the directory
-        initialize_ff_models(opts, ff_models, load_ff_datas)
-
+        initialize_ff_models(opts, models, load_ff_datas) # feed forwad models from the directory
+    
+    print('models: ', models)
+    
     # Initialize baseline models
     baseline_models = []
     for i in range(len(opts.eval_baselines)):
@@ -358,6 +354,8 @@ def run(opts):
         for m in range(len(models)):  # Get the performance of the trained models
             ops = get_op_ratios(opts, models[m], problem)
             trained_models_results.append(ops[m])
+        #print('baseline_results[0]: ', baseline_results[0])
+        #print('trained_models_results ', trained_models_results)
         results = np.array([baseline_results[0], trained_models_results])
         torch.save(
             torch.tensor(results),
