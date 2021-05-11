919ac42cf02e9a1607a9dc5de198bad0afd62193
Mohammad Ali Alomrani <alomrani@cedar1.cedar.computecanada.ca>
2021-05-11 06:36:40 -0700

    added gmission variant

--- data/generate_data.py
+++ data/generate_data.py
@@ -217,7 +217,7 @@ def parse_gmission_dataset():
 
 
 def generate_gmission_graph(
-    u, v, tasks, edges, workers, p, seed, weight_dist, weight_param
+    u, v, tasks, edges, workers, p, seed, weight_dist, weight_param, vary_fixed=False
 ):
     np.random.seed(seed)
 
@@ -225,7 +225,8 @@ def generate_gmission_graph(
     G = _add_nodes_with_bipartite_label(G, u, v)
 
     G.name = f"gmission_random_graph({u},{v})"
-
+    if vary_fixed:
+        workers = list(np.random.randint(1, 533, size=u_size))
     availableWorkers = workers.copy()
     weights = []
     for i in range(v):
@@ -369,6 +370,13 @@ def generate_edge_obm_data_geometric(
         np.random.seed(100)
         workers = list(np.random.randint(1, 533, size=u_size))
         g = generate_gmission_graph
+    elif graph_family == "gmission-var":
+        edges, tasks = parse_gmission_dataset()
+        max_w = max(np.array(list(edges.values()), dtype="float"))
+        edges = {k: (float(v) / float(max_w)) for k, v in edges.items()}
+        #np.random.seed(100)
+        #workers = list(np.random.randint(1, 533, size=u_size))
+        g = lambda u_size, v_size, tasks, edges, workers, graph_family_parameter, seed + i, weight_distribution, weight_param : generate_gmission_graph(u_size, v_size, tasks, edges, workers, graph_family_parameter, seed + i, weight_distribution, weight_param, vary_fixed=True)
     for i in tqdm(range(dataset_size)):
         g1, weights, w = g(
             u_size,
