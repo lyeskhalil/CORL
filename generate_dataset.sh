e1b752b997d9f57c1579b815c560a80c72fe5df9
alomrani <mohammad.alomrani@mail.utoronto.ca>
2021-05-12 13:56:46 -0400

    pull

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -5,33 +5,33 @@
 
 U_SIZE=$1
 V_SIZE=$2
-GRAPH_FAMILY="er"
+GRAPH_FAMILY="gmission"
 FAMILY_PARAMETER=$3
 PROBLEM="e-obm"
 TRAIN_SIZE=10000
 VAL_SIZE=1000
 MAX_WEIGHT=100
-WEIGHT_DIST="fixed-normal"
-MEAN=5
-VARIANCE=100
+WEIGHT_DIST="gmission"
+MEAN=-1
+VARIANCE=-1
 a=3
 DATASET="$PROBLEM"_"$GRAPH_FAMILY"_"$U_SIZE"by"$V_SIZE"_"p=$FAMILY_PARAMETER"_"$WEIGHT_DIST"_"m=$MEAN"_"v=$VARIANCE"_"a=$a"
 
-module load python/3.6
-module load scipy-stack
+#module load python/3.6
+#module load scipy-stack
 
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
-pip install --no-index --upgrade pip
+#virtualenv --no-download $SLURM_TMPDIR/env
+#source $SLURM_TMPDIR/env/bin/activate
+#pip install --no-index --upgrade pip
 
-pip install --no-index -r requirements.txt
+#pip install --no-index -r requirements.txt
 
 
 # python data/generate_data.py --dataset_size $TRAIN_SIZE --seed 50000 --u_size $U_SIZE --v_size $V_SIZE --graph_family_parameter $FAMILY_PARAMETER  --dataset_folder $SLURM_TMPDIR/$DATASET/train
-python data/generate_data.py --weight_distribution_param $MEAN $VARIANCE --weight_distribution $WEIGHT_DIST --graph_family $GRAPH_FAMILY --problem $PROBLEM --dataset_size $TRAIN_SIZE --u_size $U_SIZE --v_size $V_SIZE --graph_family_parameter $FAMILY_PARAMETER --dataset_folder $SLURM_TMPDIR/$DATASET/train
+python data/generate_data.py --weight_distribution_param $MEAN $VARIANCE --weight_distribution $WEIGHT_DIST --graph_family $GRAPH_FAMILY --problem $PROBLEM --dataset_size $TRAIN_SIZE --u_size $U_SIZE --v_size $V_SIZE --graph_family_parameter $FAMILY_PARAMETER --dataset_folder $DATASET/train
 
 python data/generate_data.py --problem $PROBLEM --weight_distribution_param $MEAN $VARIANCE --weight_distribution $WEIGHT_DIST --graph_family $GRAPH_FAMILY --dataset_size $VAL_SIZE --u_size $U_SIZE --v_size $V_SIZE --graph_family_parameter $FAMILY_PARAMETER --seed 20000 --dataset_folder $SLURM_TMPDIR/$DATASET/val
 
-cd $SLURM_TMPDIR
+#cd $SLURM_TMPDIR
 
-tar cf ~/projects/def-khalile2/alomrani/$DATASET.tar $DATASET
+#tar cf ~/projects/def-khalile2/alomrani/$DATASET.tar $DATASET
