780d7a77a8521d27c77e2deba90a0ccf337c0f11
Mohammad Ali Alomrani <alomrani@cdr2585.int.cedar.computecanada.ca>
2021-05-18 11:17:21 -0700

    pull

--- scripts/train_jobs.sh
+++ scripts/train_jobs.sh
@@ -4,16 +4,16 @@ U_SIZE=$1
 V_SIZE=$2
 GRAPH_FAMILY="gmission"
 DIST="gmission"
-MODEL="inv-ff-hist"
+MODEL="inv-ff"
 MEAN=-1
 VAR=-1
 LR=0.02
-LR_DECAY=0.98
-EXP_BETA=0.65
-ENT_RATE=0.02
+LR_DECAY=0.96
+EXP_BETA=0.75
+
+
 
 for i in "-1" 
 do
-    sbatch --account=def-khalile2 generate_dataset.sh $U_SIZE $V_SIZE $i 
-    #sbatch --account=def-khalile2 train.sh $U_SIZE $V_SIZE $i $GRAPH_FAMILY $DIST $MEAN $VAR $MODEL $LR $LR_DECAY $EXP_BETA $ENT_RATE 
+    sbatch --account=def-khalile2 train.sh $U_SIZE $V_SIZE $i $GRAPH_FAMILY $DIST $MEAN $VAR $MODEL $LR $LR_DECAY $EXP_BETA 
 done
