1672ce60cc44ebfba5ec1351decedba3fbe5249d
Mohammad Ali Alomrani <omrani2005@hotmail.com>
2022-01-27 22:50:10 -0500

    de-anon repo

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version="0.1.0",
     description="Exploring Online Bipartite Matching using Reinforcement Learning (CORL)",
-    author="Mohammad Ali Alomrani",
+    author="Anon",
     license="MIT",
 )
